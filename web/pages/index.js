20be8fec260b956bd9de57cbf1cbd75395f4f8f1
youngeunkim373 <youngeunkim373@gmail.com>
2022-10-05 21:01:02 +0900

    test5

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,4 +1,57 @@
 import Head from "next/head";
+import prisma from "../lib/prisma";
+
+/*---------- Read ----------*/
+export async function getServerSideProps(context) {
+  // prisma
+  const data = await prisma.members.findMany({});
+  const members = data.map((member) => ({
+    ...member,
+  }));
+
+  // raw SQL
+  // const dataFromSql = await prisma.$queryRaw`
+  //   SELECT  A.user_email    AS user_email
+  //          ,A.user_nicknm   AS user_nicknm
+  //          ,A.user_pw       AS user_pw
+  //          ,A.user_sex      AS user_sex
+  //          ,A.user_age      AS user_age
+  //          ,A.user_region   AS user_region
+  //          ,A.user_img      AS user_img
+  //          ,A.reg_id        AS reg_id
+  //          ,A.reg_date      AS reg_date
+  //          ,A.reg_time      AS reg_time
+  //     FROM Members A
+  // `;
+  // const membersFromSql = dataFromSql.map((memberFromSql) => ({
+  //   ...memberFromSql,
+  // }));
+
+  // const newProducts = await prisma.product.createMany({
+  //   data: [
+  //     {
+  //       id: 4,
+  //       name: "테스트 모자",
+  //       description: "테스트 모자입니다.",
+  //       price: 10000,
+  //       // image: null,
+  //       category_id: 1,
+  //     },
+  //     {
+  //       id: 5,
+  //       name: "테스트 모자2",
+  //       description: "테스트 모자2입니다.",
+  //       price: 20000,
+  //       // image: null,
+  //       category_id: 2,
+  //     },
+  //   ],
+  // });
+
+  return {
+    props: { members },
+  };
+}
 
 export default function Home({ members, membersFromSql }) {
   return (
@@ -103,64 +156,4 @@ export default function Home({ members, membersFromSql }) {
       </div>
     </div>
   );
-}
-
-/*---------- Read ----------*/
-export async function getStaticProps(context) {
-  // prisma
-  const data = [
-    {
-      user_email: "1",
-      user_nicknm: "2",
-      user_sex: "F",
-      user_age: "3",
-      user_region: "4",
-    },
-  ];
-  const members = data.map((member) => ({
-    ...member,
-  }));
-
-  // raw SQL
-  // const dataFromSql = await prisma.$queryRaw`
-  //   SELECT  A.user_email    AS user_email
-  //          ,A.user_nicknm   AS user_nicknm
-  //          ,A.user_pw       AS user_pw
-  //          ,A.user_sex      AS user_sex
-  //          ,A.user_age      AS user_age
-  //          ,A.user_region   AS user_region
-  //          ,A.user_img      AS user_img
-  //          ,A.reg_id        AS reg_id
-  //          ,A.reg_date      AS reg_date
-  //          ,A.reg_time      AS reg_time
-  //     FROM Members A
-  // `;
-  // const membersFromSql = dataFromSql.map((memberFromSql) => ({
-  //   ...memberFromSql,
-  // }));
-
-  // const newProducts = await prisma.product.createMany({
-  //   data: [
-  //     {
-  //       id: 4,
-  //       name: "테스트 모자",
-  //       description: "테스트 모자입니다.",
-  //       price: 10000,
-  //       // image: null,
-  //       category_id: 1,
-  //     },
-  //     {
-  //       id: 5,
-  //       name: "테스트 모자2",
-  //       description: "테스트 모자2입니다.",
-  //       price: 20000,
-  //       // image: null,
-  //       category_id: 2,
-  //     },
-  //   ],
-  // });
-
-  return {
-    props: { members },
-  };
 }
