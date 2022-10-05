c62d250a209f9549fb2b2f68a47621420a0ff5c8
youngeunkim373 <youngeunkim373@gmail.com>
2022-10-05 17:23:04 +0900

    test3

--- web/pages/index.js
+++ web/pages/index.js
@@ -24,7 +24,7 @@ export default function Home({ members, membersFromSql }) {
               </tr>
             </thead>
             <tbody>
-              {members.map((member) => (
+              {/* {members.map((member) => (
                 <tr key={member.id}>
                   <td style={{ border: "1px solid #444444" }}>
                     {member.user_email}
@@ -42,7 +42,7 @@ export default function Home({ members, membersFromSql }) {
                     {member.user_region}
                   </td>
                 </tr>
-              ))}
+              ))} */}
             </tbody>
           </table>
           <h4>Raw SQL</h4>
@@ -57,7 +57,7 @@ export default function Home({ members, membersFromSql }) {
               </tr>
             </thead>
             <tbody>
-              {membersFromSql.map((member) => (
+              {/* {membersFromSql.map((member) => (
                 <tr key={member.id}>
                   <td style={{ border: "1px solid #444444" }}>
                     {member.user_email}
@@ -75,7 +75,7 @@ export default function Home({ members, membersFromSql }) {
                     {member.user_region}
                   </td>
                 </tr>
-              ))}
+              ))} */}
             </tbody>
           </table>
         </div>
@@ -107,53 +107,53 @@ export default function Home({ members, membersFromSql }) {
 }
 
 /*---------- Read ----------*/
-export async function getStaticProps(context) {
-  // prisma
-  const data = await prisma.members.findMany({});
-  const members = data.map((member) => ({
-    ...member,
-  }));
+// export async function getStaticProps(context) {
+//   // prisma
+//   const data = await prisma.members.findMany({});
+//   const members = data.map((member) => ({
+//     ...member,
+//   }));
 
-  // raw SQL
-  const dataFromSql = await prisma.$queryRaw`
-    SELECT  A.user_email    AS user_email 
-           ,A.user_nicknm   AS user_nicknm
-           ,A.user_pw       AS user_pw    
-           ,A.user_sex      AS user_sex   
-           ,A.user_age      AS user_age   
-           ,A.user_region   AS user_region
-           ,A.user_img      AS user_img   
-           ,A.reg_id        AS reg_id     
-           ,A.reg_date      AS reg_date   
-           ,A.reg_time      AS reg_time             
-      FROM Members A
-  `;
-  const membersFromSql = dataFromSql.map((memberFromSql) => ({
-    ...memberFromSql,
-  }));
+//   // raw SQL
+//   const dataFromSql = await prisma.$queryRaw`
+//     SELECT  A.user_email    AS user_email
+//            ,A.user_nicknm   AS user_nicknm
+//            ,A.user_pw       AS user_pw
+//            ,A.user_sex      AS user_sex
+//            ,A.user_age      AS user_age
+//            ,A.user_region   AS user_region
+//            ,A.user_img      AS user_img
+//            ,A.reg_id        AS reg_id
+//            ,A.reg_date      AS reg_date
+//            ,A.reg_time      AS reg_time
+//       FROM Members A
+//   `;
+//   const membersFromSql = dataFromSql.map((memberFromSql) => ({
+//     ...memberFromSql,
+//   }));
 
-  // const newProducts = await prisma.product.createMany({
-  //   data: [
-  //     {
-  //       id: 4,
-  //       name: "테스트 모자",
-  //       description: "테스트 모자입니다.",
-  //       price: 10000,
-  //       // image: null,
-  //       category_id: 1,
-  //     },
-  //     {
-  //       id: 5,
-  //       name: "테스트 모자2",
-  //       description: "테스트 모자2입니다.",
-  //       price: 20000,
-  //       // image: null,
-  //       category_id: 2,
-  //     },
-  //   ],
-  // });
+//   // const newProducts = await prisma.product.createMany({
+//   //   data: [
+//   //     {
+//   //       id: 4,
+//   //       name: "테스트 모자",
+//   //       description: "테스트 모자입니다.",
+//   //       price: 10000,
+//   //       // image: null,
+//   //       category_id: 1,
+//   //     },
+//   //     {
+//   //       id: 5,
+//   //       name: "테스트 모자2",
+//   //       description: "테스트 모자2입니다.",
+//   //       price: 20000,
+//   //       // image: null,
+//   //       category_id: 2,
+//   //     },
+//   //   ],
+//   // });
 
-  return {
-    props: { members, membersFromSql },
-  };
-}
+//   return {
+//     props: { members, membersFromSql },
+//   };
+// }
