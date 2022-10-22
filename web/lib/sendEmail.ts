ce5c7a1280ee9bf48a1d7f6339f30b094f5e2a99
youngeunkim373 <youngeunkim373@gmail.com>
2022-10-22 13:58:48 +0900

    신규: nodemailer 메일 기본 양식 만들기
    1) sendEmail에 html 양식 작성
    2) api > members에서 text 수정

--- web/lib/sendEmail.ts
+++ web/lib/sendEmail.ts
@@ -1,12 +1,12 @@
 const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
-  service: "gmail", //사용하고자 하는 서비스
+  service: "gmail",
   prot: 587,
   host: "smtp.gmlail.com",
   secure: false,
   requireTLS: true,
   auth: {
-    user: "youngeunkim373@gmail.com", //gmail주소입력
+    user: "youngeunkim373@gmail.com",
     pass: "fmkmcrqylhfwhbav",
   },
 });
@@ -17,11 +17,96 @@ export async function sendEmail(
   subject: string,
   text: string
 ) {
+  const html = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700&display=swap"
+        rel="stylesheet"
+      />
+      <link
+        rel="stylesheet"
+        type="text/css"
+        href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css"
+      />
+    </head>
+    <body>
+      <div>
+        <table
+          cellpadding="0"
+          width="600px"
+          cellspacing="50"
+          style="border: 1px solid #eaeaea"
+        >
+          <tbody>
+            <tr>
+              <td>
+                <a href="https://passersby-pvmf1a0k3-youngeunkim373.vercel.app/">
+                  <img
+                    src="${process.env.NEXT_PUBLIC_ENV_HOST}/images/logo+title.png"
+                    width="200"
+                    alt="Logo"
+                  />
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <h1
+                  style="
+                    font-family: 'Pretendard-Regular';
+                    font-size: 30px;
+                    font-weight: 400;
+                    font-style: normal;
+                  "
+                >
+                  ${subject}
+                </h1>
+                <br />
+                <hr />
+              </td>
+            </tr>
+            <tr>
+              <td
+                style="
+                  font-family: 'Pretendard-Regular';
+                  font-size: 16px;
+                  font-weight: 200;
+                  font-style: normal;
+                  color: #5d5d5d;
+                  font-family: 'Nanum Gothic', sans-serif;
+                  padding-bottom: 20px;
+                "
+              >
+                ${text}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <table cellpadding="0" width="600px" cellspacing="0">
+          <tbody style="background: #eaeaea; height: 100px">
+            <tr>
+              <td align="center"><span style="color:#5d5d5d">Published by 0=2</span></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </body>
+  </html>
+  
+  `;
+
   const info = transporter.sendMail({
     from: from,
     to: to,
     subject: subject,
-    text: text,
+    html: html,
   });
 
   return info;
--- web/pages/api/members.ts
+++ web/pages/api/members.ts
@@ -31,8 +31,11 @@ export default async function members(
 
           const from = "youngeunkim373@gmail.com";
           const subject = "길 가는 사람들 본인인증 메일";
-          const text = `길 가는 사람들 본인인증 메일입니다. 아래의 번호를 회원가입 창의 인증번호란에 입력해주세요.
-                       ${verifyNumber}`;
+          const text = `길 가는 사람들 본인인증 메일입니다.<br/>
+                        길 가는 사람들의 회원이 되길 희망해주셔서 감사합니다.<br/>
+                        아래의 번호를 회원가입 화면의 인증번호란에 입력해주세요.<br/><br/>
+                        인증번호: <b style="color: #9000ff"> ${verifyNumber}</b>
+                       `;
           sendEmail(from, email, subject, text);
         }
 
