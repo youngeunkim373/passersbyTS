68f2a453b2a564eb7a2e717cac9a206c4b6dcc81
youngeunkim373 <youngeunkim373@gmail.com>
2022-11-01 01:59:11 +0900

    신규: boardWrite.tsx 화면 설계
    1. board > index.tsx에 글쓰기 버튼 작성
    - 글쓰기 버튼 작성
    - 로그인해야 글쓸 수 있도록 조건 추가
    2. boardWrite.tsx 화면 작성
    3. 기타
    - public > images > crossedFlags.png 저장
    - GlobalStyles.ts에 #editor-page 추가
    - GlobalStyles.ts에서 CLASS 부분 삭제
    - GlobalStyles.ts에서.quill-container에 background 추가
    - member.module.css 삭제

--- web/pages/board/boardWrite.tsx
+++ web/pages/board/boardWrite.tsx
@@ -1,5 +1,242 @@
+import React, { useRef, useState } from "react";
+import styled from "styled-components";
+
+import CloseIcon from "@mui/icons-material/Close";
+import SportsScoreIcon from "@mui/icons-material/SportsScore";
+
+import Alert from "../../components/molecules/alert";
+import BasicButton from "../../components/atoms/basicButton";
+import BasicInput from "../../components/atoms/basicInput";
+import Editor from "../../components/organisms/editor";
+import PushButton from "../../components/atoms/pushButton";
+import Title from "../../components/atoms/title";
+import Image from "next/image";
+
+interface InputItem {
+  id: number;
+}
+
 const BoardWrite = () => {
-  return <div>게시글 쓰기</div>;
+  const [alert, setAlert] = useState({ open: false, text: "" });
+  const [choices, setChoices] = React.useState<InputItem[]>([]);
+  const [yesOrNos, setYesOrNos] = React.useState<InputItem[]>([]);
+
+  const inputId = useRef<number>(1);
+
+  function addInput(): void {
+    if (yesOrNos.length !== 0) {
+      setAlert({
+        open: true,
+        text: "N지선다 선택지와 찬반 선택지는 함께 생성할 수 없습니다.",
+      });
+      return;
+    }
+
+    if (choices.length >= 5) {
+      setAlert({
+        open: true,
+        text: "N지선다 선택지는 5개까지 생성할 수 있습니다.",
+      });
+      return;
+    }
+
+    const newChoice = {
+      id: inputId.current,
+    };
+
+    setChoices([...choices, newChoice]);
+    inputId.current += 1;
+  }
+
+  function removeInput(index: number): void {
+    setChoices(choices.filter((choice) => choice.id !== index));
+  }
+
+  function addButton(): void {
+    if (choices.length !== 0) {
+      setAlert({
+        open: true,
+        text: "N지선다 선택지와 찬반 선택지는 함께 생성할 수 없습니다.",
+      });
+      return;
+    }
+
+    if (yesOrNos.length !== 0) {
+      setAlert({
+        open: true,
+        text: "찬반 선택지는 한 번만 생성할 수 있습니다.",
+      });
+      return;
+    }
+    const newYesOrNo = {
+      id: inputId.current,
+    };
+
+    setYesOrNos([...yesOrNos, newYesOrNo]);
+    inputId.current = 2 * inputId.current + 1;
+  }
+
+  function removeButton(): void {
+    setYesOrNos([]);
+  }
+
+  const handleSubmit = async () => {
+    console.log("submit!");
+  };
+
+  return (
+    <form
+      id="editor-page"
+      encType="multi part/form-data"
+      onSubmit={handleSubmit}
+      method="POST"
+    >
+      <Title>게시판</Title>
+      <Alert open={alert.open} setOpen={setAlert}>
+        {alert.text}
+      </Alert>
+      <BasicContainer>
+        <BasicInput type="text" placeholder="제목을 입력하세요." width="100%" />
+      </BasicContainer>
+      <BasicContainer>
+        <BasicButton type="button" onClick={addInput}>
+          N지선다
+        </BasicButton>
+        <BasicButton type="button" onClick={addButton}>
+          찬반
+        </BasicButton>
+        <div
+          style={{
+            background: "rgba(255,255,255,0.07)",
+            borderRadius: "10px",
+            height: "auto",
+            margin: "0 auto",
+            marginTop: "30px",
+            marginBottom: "30px",
+            width: "100%",
+          }}
+        >
+          {choices &&
+            choices.map((choice: InputItem) => (
+              <ChoicesContainer key={choice.id}>
+                <InputContainer>
+                  <BasicInput
+                    type="text"
+                    placeholder="내용을 입력하세요."
+                    width="100%"
+                  />
+                </InputContainer>
+                <CloseIcon
+                  onClick={() => removeInput(choice.id)}
+                  sx={{ cursor: "pointer", float: "left", paddingLeft: "20px" }}
+                />
+              </ChoicesContainer>
+            ))}
+          {yesOrNos &&
+            yesOrNos.map((yesOrNo: InputItem) => (
+              <YesOrNosContainerWithCloseIcon key={yesOrNo.id}>
+                <YesOrNosContainer>
+                  <StyledButton type="button" color="#5f00ff">
+                    <SportsScoreIcon
+                      sx={{ color: "#5f00ff", transform: "scaleX(-1)" }}
+                    />
+                    &nbsp;찬성
+                  </StyledButton>
+                  <div style={{ float: "left" }}>
+                    <Image
+                      src="/images/crossedFlags.png"
+                      alt="flags"
+                      width="100px"
+                      height="55px"
+                    />
+                  </div>
+                  <StyledButton type="button" color="#ff0046">
+                    반대&nbsp;
+                    <SportsScoreIcon
+                      sx={{
+                        color: "#ff0046",
+                      }}
+                    />
+                  </StyledButton>
+                </YesOrNosContainer>
+                <CloseIcon
+                  onClick={removeButton}
+                  sx={{
+                    cursor: "pointer",
+                    float: "left",
+                    position: "absolute",
+                    left: "calc(50% + 180px)",
+                    top: "30%",
+                  }}
+                />
+              </YesOrNosContainerWithCloseIcon>
+            ))}
+        </div>
+      </BasicContainer>
+      <Editor />
+      <PushButtonContainer>
+        <PushButton type="submit">등록하기</PushButton>
+      </PushButtonContainer>
+    </form>
+  );
 };
 
 export default BoardWrite;
+
+const BasicContainer = styled.div`
+  padding-top: 30px;
+`;
+
+const PushButtonContainer = styled.div`
+  padding-top: 50px;
+  text-align: center;
+`;
+
+const ChoicesContainer = styled.div`
+  align-items: center;
+  display: flex;
+  padding: 10px;
+`;
+
+const YesOrNosContainer = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  margin: 0 auto;
+`;
+
+const YesOrNosContainerWithCloseIcon = styled.div`
+  position: relative;
+`;
+
+const InputContainer = styled.div`
+  float: left;
+  width: 100%;
+`;
+
+const StyledButton = styled.button`
+  align-items: center;
+  background: ${({ color }) => color};
+  border: ${({ color }) => "3px solid" + color};
+  border-radius: 30px;
+  color: white;
+  display: flex;
+  float: left;
+  font-family: ibmRegular;
+  font-size: 20px;
+  font-weight: bold;
+  padding: 8px 20px;
+  svg {
+    color: white;
+  }
+
+  &:hover {
+    background: white;
+    color: ${({ color }) => color};
+
+    svg {
+      color: ${({ color }) => color};
+    }
+  }
+`;
--- web/pages/board/index.tsx
+++ web/pages/board/index.tsx
@@ -1,6 +1,10 @@
 import { useEffect, useRef, useState } from "react";
+import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
 import axios from "axios";
 import styled from "styled-components";
+
+import CreateIcon from "@mui/icons-material/Create";
 
 import Alert from "../../components/molecules/alert";
 import BasicSelect from "../../components/atoms/basicSelect";
@@ -8,6 +12,7 @@ import SearchBar from "../../components/molecules/searchBar";
 import Title from "../../components/atoms/title";
 import List from "../../components/organisms/list";
 import Pagination from "../../components/molecules/pagination";
+import PushButton from "../../components/atoms/pushButton";
 
 const criteriaList = {
   registerDate: "최신순",
@@ -25,7 +30,22 @@ const BoardList = (props: any) => {
   );
   const [search, setSearch] = useState("");
 
+  const router = useRouter();
+
+  const { data: session, status } = useSession();
+
   const criteriaRef = useRef<HTMLLabelElement>(null);
+
+  const onButtonClick = async (e: React.MouseEvent) => {
+    if (status !== "authenticated") {
+      setAlert({
+        open: true,
+        text: "로그인 후 글쓰기 가능합니다.",
+      });
+      return;
+    }
+    router.push("board/boardWrite");
+  };
 
   useEffect(() => {
     async function fetchBoardList() {
@@ -85,6 +105,12 @@ const BoardList = (props: any) => {
           setCurrentPage={setCurrentPage}
         />
       </PaginationContainer>
+      <PushButtonContainer>
+        <PushButton type="button" onClick={onButtonClick}>
+          <CreateIcon />
+          글쓰기
+        </PushButton>
+      </PushButtonContainer>
     </div>
   );
 };
@@ -120,4 +146,11 @@ const SearchContainer = styled.div`
 
 const PaginationContainer = styled.div`
   padding-top: 30px;
+`;
+
+const PushButtonContainer = styled.div`
+  position: fixed;
+  top: 85%;
+  right: 4.5%;
+  z-index: 1;
 `;
